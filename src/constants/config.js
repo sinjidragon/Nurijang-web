3c10b6709d0ede60602aa170413b1154fc795ad7
iszero12 <binj1246@gmail.com>
2024-12-01 21:50:54 +0900

    fix :: Hiding API and URL

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyBR3XR176Wj4TBWWcH2qY_tungtmqpofVw';
+export const GOOGLE_MAPS_API_KEY = '${GOOGLE_MAPS_API_KEY}';
 
 export const MAP_STYLES = {
   height: "100vh",
