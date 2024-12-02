94576c5c1cd4af62750de4a9312a29ea79da1121
iszero12 <binj1246@gmail.com>
2024-12-02 11:04:17 +0900

    Edit api key reference

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ChatComponent from './components/ChatComponent';
 import './App.css';
 
 const App = () => {
+  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const { 
     center, setCenter, facilities, setFacilities, 
     userLocation, setUserLocation, loading, setLoading, 
@@ -152,7 +153,7 @@ const App = () => {
         </button>
       </div>
 
-      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
+      <LoadScript googleMapsApiKey={apiKey}>
         <GoogleMap
           mapContainerStyle={MAP_STYLES}
           zoom={15}
